37fefc3708604eb31786530052b2f40bc0ae8249
MohanadArkia <arkia.ar.47@gmail.com>
2024-03-28 03:58:44 +0200

    Added lock button/functionality

--- Main.c
+++ Main.c
@@ -22,6 +22,12 @@ void sleep_button_clicked(GtkWidget *widget, gpointer data)
     system("systemctl suspend");
 }
 
+void lock_button_clicked(GtkWidget *widget, gpointer data)
+{
+    gtk_main_quit();
+    system("xscreensaver-command -lock");
+}
+
 int main(int argc, char *argv[])
 {
     // Initialize GTK
@@ -41,29 +47,34 @@ int main(int argc, char *argv[])
     GtkWidget *shutdown_image = gtk_image_new_from_file("./images/shutdown.png");
     GtkWidget *reboot_image = gtk_image_new_from_file("./images/reboot.png");
     GtkWidget *sleep_image = gtk_image_new_from_file("./images/sleep.png");
+    GtkWidget *lock_image = gtk_image_new_from_file("./images/lock.png");
 
     // Create buttons and set images
     GtkWidget *cancel_button = gtk_button_new();
     GtkWidget *shutdown_button = gtk_button_new();
     GtkWidget *reboot_button = gtk_button_new();
     GtkWidget *sleep_button = gtk_button_new();
+    GtkWidget *lock_button = gtk_button_new();
 
     gtk_button_set_image(GTK_BUTTON(cancel_button), cancel_image);
     gtk_button_set_image(GTK_BUTTON(shutdown_button), shutdown_image);
     gtk_button_set_image(GTK_BUTTON(reboot_button), reboot_image);
     gtk_button_set_image(GTK_BUTTON(sleep_button), sleep_image);
+    gtk_button_set_image(GTK_BUTTON(lock_button), lock_image);
 
     // Connect the clicked signals of all buttons to their respective callback functions
     g_signal_connect(cancel_button, "clicked", G_CALLBACK(cancel_button_clicked), NULL);
     g_signal_connect(sleep_button, "clicked", G_CALLBACK(sleep_button_clicked), NULL);
     g_signal_connect(reboot_button, "clicked", G_CALLBACK(reboot_button_clicked), NULL);
     g_signal_connect(shutdown_button, "clicked", G_CALLBACK(shutdown_button_clicked), NULL);
+    g_signal_connect(lock_button, "clicked", G_CALLBACK(lock_button_clicked), NULL);
 
     // Add buttons to the horizontal box
     gtk_box_pack_start(GTK_BOX(hbox), cancel_button, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(hbox), shutdown_button, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(hbox), reboot_button, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(hbox), sleep_button, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(hbox), lock_button, TRUE, TRUE, 0);
 
     // Add the horizontal box to the window
     gtk_container_add(GTK_CONTAINER(window), hbox);
